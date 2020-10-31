5427bb7eac4035897d8fb2d8950808542dfb646e
高春华 <gaochunhua@qiyi.com>
2020-10-31 22:37:30 +0800

    update

--- xchange-core/src/main/java/org/knowm/xchange/utils/PositionCalcUtils.java
+++ xchange-core/src/main/java/org/knowm/xchange/utils/PositionCalcUtils.java
@@ -89,6 +89,10 @@ public class PositionCalcUtils {
       public BigDecimal getPnlRate() {
         return pnlRate;
       }
+
+      public BigDecimal getPriceDiff() {
+        return priceDiff;
+      }
     }
 
 
